c58fa05f67ab3b291dcf1636907fb75134bdc4f5
Kiryl Pastukhou <92473617+AlabaAclydiem@users.noreply.github.com>
2024-07-16 06:55:39 +0300

    adding houros part1

--- outfit_matcher.py
+++ outfit_matcher.py
@@ -11,35 +11,46 @@ class OutfitMatcher:
         self.model = OutfitModel()
         self.weather_api = WeatherCityInfo()
 
-    def match(self, weather_func, *args):
+    def match(self, include_hours, weather_func, args=None):
         return self.model.predict(
             clothes=matcher.dataset.to_json(),
             outfit=matcher.dataset.get_outfit(),
-            weather=weather_func(*args),
+            weather=weather_func(*args),  
+                  
         )
 
     def start(self):
         while True:
-            command = input("Enter a command (now, today, YYYY-MM-DD, exit):\n")
-            if command == "exit":
-                print("Exiting...")
-                break
-            elif command == "now":
-                outfit = self.match(self.weather_api.get_current_weather)
-                print(outfit)
-                self.dataset.save_outfit(self.model.extract_outfit(outfit))
-            elif command == "today":
-                outfit = self.match(self.weather_api.get_today_forecast)
-                print(outfit)
-                self.dataset.save_outfit(self.model.extract_outfit(outfit))
-            else:
-                try:
-                    datetime.strptime(command, "%Y-%m-%d")
-                    outfit = self.match(self.weather_api.get_day_forecast, command)
+            command = input("Enter a command (now, today, YYYY-MM-DD, exit) or setting option (city, hours):\n")
+            match command:
+                case "city":
+                    city = input('Enter a city name:\n')
+                    try:
+                        self.weather_api.set_city(city)
+                    except ValueError as e:
+                        print(e)
+                case "hours":
+                    hours = tuple(mao(int, input("Enter two numbers between 0 and 23 separated by space").split()))
+                    
+                case "exit":
+                    print("Exiting...")
+                    break
+                case "now":
+                    outfit = self.match(include_hours=False, weather_func=self.weather_api.get_current_weather)
                     print(outfit)
                     self.dataset.save_outfit(self.model.extract_outfit(outfit))
-                except ValueError:
-                    print("Wrong command")
+                case "today":
+                    outfit = self.match(include_hours=True, weather_func=self.weather_api.get_today_forecast)
+                    print(outfit)
+                    self.dataset.save_outfit(self.model.extract_outfit(outfit))
+                case _:
+                    try:
+                        datetime.strptime(command, "%Y-%m-%d")
+                        outfit = self.match(include_hours=False, weather_func=self.weather_api.get_day_forecast, args=[command])
+                        print(outfit)
+                        self.dataset.save_outfit(self.model.extract_outfit(outfit))
+                    except ValueError:
+                        print("Wrong command")
 
 
 if __name__ == "__main__":
--- weather_city_info.py
+++ weather_city_info.py
@@ -9,17 +9,30 @@ from weather_urls import (
 
 
 class WeatherCityInfo:
-    def __init__(self, city="Minsk") -> None:
+    def __init__(self, city="Minsk", preffered_hours=(9, 20)) -> None:
         self.city = city
         self.longitude = None
         self.latitude = None
+        self.preferred_hours = preffered_hours
         self._set_city_coordinates()
+        self._check_hours()
 
+    def _check_hours(self):
+        if self.preferred_hours[0] > self.preferred_hours[1]:
+            self.preferred_hours = (self.preferred_hours[1], self.preferred_hours[0])
+        else:
+            self.preferred_hours = (self.preferred_hours[0], self.preferred_hours[1])
+        if self.preferred_hours[0] < 0 or self.preferred_hours[1] > 23:
+            raise ValueError('Incorret day hours')
+            
     def _set_city_coordinates(self):
         url = CITY_LATLONG_URL.format(self.city)
         response = requests.get(url)
         if response.status_code == 200:
-            data = response.json()["results"][0]
+            try:
+                data = response.json()["results"][0]
+            except KeyError:
+                raise ValueError("Nonexisting city")
             self.latitude = data["latitude"]
             self.longitude = data["longitude"]
         else:
@@ -30,6 +43,10 @@ class WeatherCityInfo:
         if response.status_code == 200:
             return response.json()
 
+    def set_hours(self, preffered_hours: tuple[int, int]): 
+        self.preferred_hours = preffered_hours
+        self._check_hours()
+    
     def set_city(self, city: str) -> None:
         self.city = city
         self._set_city_coordinates()
@@ -45,3 +62,4 @@ class WeatherCityInfo:
     def get_day_forecast(self, day: str) -> None:
         url = WEATHER_DAY_URL.format(self.latitude, self.longitude, day, day)
         return self._get_json(requests.get(url))
+
